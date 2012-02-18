c92e8f127ce0fa7f52ea0640917e052f96fb815d
Travis Graham <t@tgraham.us>
2012-02-18 16:36:28 -0500

    Initial Commit

---
+++ lib/moonshine/passenger_monitor.rb
@@ -0,0 +1,30 @@
+module Moonhshine
+  module PassengerMonitor
+    def monitor_template_dir
+      Pathname.new(__FILE__).dirname.join('passenger_monitor', 'templates')
+    end
+    
+    def passenger_monitor
+      if configuration[:passenger]
+        file '/usr/local/bin/passenger-memory-monitor',
+              :ensure => :present,
+              :content => template(monitor_template_dir.join('passenger-memory-monitor'), binding),
+          	  :mode => '755'
+        
+        if options[:cron] != false
+          options[:cron] ||= {}
+          cron 'passenger_memory_monitor',
+            :command => "/usr/local/bin/passenger-memory-monitor #{options[:memory]} >> /var/log/passenger-memory-monitor.log",
+            :minute => options[:cron][:minute] || '*/1',
+            :hour => options[:cron][:hour] || '*',
+            :monthday => options[:cron][:monthday] || '*',
+            :month => options[:cron][:month] || '*',
+            :weekday => options[:cron][:weekday] || '*',
+            :user => :root
+        else
+          cron 'passenger_memory_monitor', :ensure => :absent
+        end
+      end
+    end
+  end
+end
